7fa6c9678a98ab33e5dcae742f8b7dee6139b0a7
alessanderleite <alessander.logistica@gmail.com>
2019-01-26 10:47:55 -0200

    make the class RecordingAdapter extends RecyclerView and implement the methods

--- app/src/main/java/br/com/alessanderleite/voicerecorderapp/RecordingAdapter.java
+++ app/src/main/java/br/com/alessanderleite/voicerecorderapp/RecordingAdapter.java
@@ -1,4 +1,46 @@
 package br.com.alessanderleite.voicerecorderapp;
 
-public class RecordingAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.ViewHolder> {
+
+    private ArrayList<Recording> recordingArrayList;
+    private Context context;
+
+    public RecordingAdapter(Context context, ArrayList<Recording> recordingArrayList) {
+        this.context = context;
+        this.recordingArrayList = recordingArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recording_item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return recordingArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
