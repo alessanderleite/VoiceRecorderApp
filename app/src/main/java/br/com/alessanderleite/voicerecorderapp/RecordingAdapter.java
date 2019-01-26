cf806f3c9b55348e1fb76688f07f3ab6743cfdaf
alessanderleite <alessander.logistica@gmail.com>
2019-01-26 11:56:30 -0200

    code the inner class ViewHolder inside the RecordingAdapter class

--- app/src/main/java/br/com/alessanderleite/voicerecorderapp/RecordingAdapter.java
+++ app/src/main/java/br/com/alessanderleite/voicerecorderapp/RecordingAdapter.java
@@ -1,19 +1,28 @@
 package br.com.alessanderleite.voicerecorderapp;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.ViewHolder> {
 
-    private ArrayList<Recording> recordingArrayList;
     private Context context;
+    private ArrayList<Recording> recordingArrayList;
+    private MediaPlayer mPlayer;
+    private boolean isPlaying = false;
+    private int last_index = -1;
 
     public RecordingAdapter(Context context, ArrayList<Recording> recordingArrayList) {
         this.context = context;
@@ -39,8 +48,100 @@ public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.View
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView imageViewPlay;
+        SeekBar seekBar;
+        TextView textViewName;
+        private String recordingUri;
+        private int lastProgress = 0;
+        private Handler mHandler = new Handler();
+        ViewHolder holder;
+
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageViewPlay = (ImageView) itemView.findViewById(R.id.imageViewPlay);
+            seekBar = (SeekBar) itemView.findViewById(R.id.seekBar);
+            textViewName = (TextView) itemView.findViewById(R.id.textViewRecordingName);
+        }
+
+        public void manageSeekBar(ViewHolder holder) {
+            holder.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    if (mPlayer != null && fromUser) {
+                        mPlayer.seekTo(progress);
+                    }
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+        }
+
+        private void markAllPaused() {
+            for (int i = 0; i < recordingArrayList.size(); i++) {
+                recordingArrayList.get(i).setPlaying(false);
+                recordingArrayList.set(i, recordingArrayList.get(i));
+            }
+            notifyDataSetChanged();
+        }
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                seekUpdation(holder);
+            }
+        };
+
+        private void seekUpdation(ViewHolder holder) {
+            this.holder = holder;
+            if (mPlayer != null) {
+                int mCurrentPosition = mPlayer.getCurrentPosition();
+                holder.seekBar.setMax(mPlayer.getDuration());
+                holder.seekBar.setProgress(mCurrentPosition);
+                lastProgress = mCurrentPosition;
+            }
+            mHandler.postDelayed(runnable, 100);
+        }
+
+        private void stopPlaying() {
+            try {
+                mPlayer.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            mPlayer = null;
+            isPlaying = false;
+        }
+
+        private void startPlaying(final Recording audio, final int position) {
+            mPlayer = new MediaPlayer();
+            try {
+                mPlayer.setDataSource(recordingUri);
+                mPlayer.prepare();
+                mPlayer.start();
+            } catch (IOException e) {
+                Log.e("LOG_TAG", "prepare() failed");
+            }
+            //showing the pause button
+            seekBar.setMax(mPlayer.getDuration());
+            isPlaying = true;
+
+            mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    audio.setPlaying(false);
+                    notifyItemChanged(position);
+                }
+            });
         }
     }
 }
