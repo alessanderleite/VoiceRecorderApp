3617240a6b7369ab5d3862a450f37e73f1e855f1
alessanderleite <alessander.logistica@gmail.com>
2019-01-26 12:35:18 -0200

    set the objects in the RecordingListActivity class

--- app/src/main/java/br/com/alessanderleite/voicerecorderapp/RecordingListActivity.java
+++ app/src/main/java/br/com/alessanderleite/voicerecorderapp/RecordingListActivity.java
@@ -2,8 +2,19 @@ package br.com.alessanderleite.voicerecorderapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class RecordingListActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerViewRecording;
+    private ArrayList<Recording> recordingArrayList;
+    private RecordingAdapter recordingAdapter;
+    private TextView textViewNoRecordings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
